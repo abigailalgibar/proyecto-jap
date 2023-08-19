f2d49bbbcf0ed89adc18a9ce16c3659fcec23d2d
abigailalgibar <abigailalgibar@gmail.com>
2023-08-19 18:11:01 -0300

    update

--- js/login.js
+++ js/login.js
@@ -1,19 +1,3 @@
-//document.addEventListener("DOMContentLoaded", function() {
-//  const form = document.querySelector("form");
-//
-//form.addEventListener("submit", function(event) {
-//event.preventDefault();
-
-//const username = document.getElementById("username").value;
-//const password = document.getElementById("password").value;
-
-//if (username.trim() === "" || password.trim() === "") {
-//alert("Por favor, completa todos los campos.");
-//} else {
-// window.location.href = "index.html"; 
-//}
-//});
-//});
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("form");
 
